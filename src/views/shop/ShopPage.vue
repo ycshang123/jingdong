43d9f1c5995ce4ae7ee76af0f28c8020ce2f83b2
ycshang <351234359@qq.com>
2022-04-27 00:15:11 +0800

    店铺详情

--- src/views/shop/ShopPage.vue
+++ src/views/shop/ShopPage.vue
@@ -1,11 +1,34 @@
 <template>
-  <div></div>
+  <div class="wrapper">
+    <div class="search">
+      <div class="iconfont search__back" @click="handleBackClick">&#xe697;</div>
+      <div class="search__content">
+        <span class="search__content__icon iconfont"></span>
+        <input placeholder="请输入商品名称" class="search__content__input" />
+      </div>
+    </div>
+    <ShopInfo :item="item" :hideBorder="true" v-show="item.imgUrl" />
+  </div>
 </template>
 
 <script setup>
-export default {
-  name: 'ShopPage',
+import { reactive, toRefs } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { get } from '../../utils/request'
+import ShopInfo from '../../components/ShopInfo.vue'
+const route = useRoute()
+const data = reactive({ item: {} })
 
+get(`/shop/${route.params.id}`).then((result) => {
+  if (result?.code === 200 && result?.data) {
+    data.item = result.data
+  }
+})
+
+const { item } = toRefs(data)
+const router = useRouter()
+const handleBackClick = () => {
+  router.back()
 }
 </script>
 
@@ -16,8 +39,38 @@ export default {
   padding: 0 0.18rem;
 }
 
-.search{
+.search {
   display: flex;
   margin: 0.14rem 0 0.04rem 0;
+  line-height: 0.32rem;
+  &__back {
+    width: 0.3rem;
+    font-size: 0.24rem;
+    color: #b6b6b6;
+  }
+  &__content {
+    display: flex;
+    flex: 1;
+    background: $search-bgColor;
+    border-radius: 0.16rem;
+    &__icon {
+      width: 0.44rem;
+      text-align: center;
+      color: $search-fontColor;
+    }
+    &__input {
+      display: block;
+      width: 100%;
+      padding-right: 0.2rem;
+      border: none;
+      background: none;
+      height: 0.32rem;
+      font-size: 0.14rem;
+      color: $content-fontcolor;
+      &::placeholder {
+        color: $content-fontcolor;
+      }
+    }
+  }
 }
 </style>
